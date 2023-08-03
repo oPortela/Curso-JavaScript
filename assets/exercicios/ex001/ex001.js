6a3da9b91bddcb31fc7259cadfb1fd0fe890f68a
Matheus Marques <matheusmarquesportela@gmail.com>
2023-08-03 16:15:06 -0300

    atualização md01 de js

---
+++ assets/exercicios/ex001/ex001.js
@@ -0,0 +1,15 @@
+/* 1.Crie uma variável para armazenar o nome do carro e ponha o valor Ferrari. E exiba-o */
+
+let nomeDoCarro = "Ferrari"
+console.log(nomeDoCarro)
+
+/* 2.Crie uma variável para armazenar o preço do bolo, que custa R$ 19,99. E exiba-o. */
+
+let precoDoBolo = 19.99
+console.log(precoDoBolo)
+
+/* 3.Corrija o código a seguir e exiba-o.
+let "cidade" =  São Paulo */ 
+
+let cidade = "São Paulo"
+console.log(cidade)
---
+++ assets/js/script.js
@@ -0,0 +1,107 @@
+//alert("Hello World")  //vai criar uma caixa de alerta.
+
+//console.log("Hello World 2.0")  //vai deixar registrado um log no devtools.
+
+/*
+let nome = "Matheus Portela"
+let idade = 20
+let cidade = "Anápolis"
+let estado = "GO"
+
+console.log(nome)
+console.log(idade)
+console.log(cidade)
+console.log(estado)
+*/
+
+/* md01 aula 06
+
+let nome = "Matheus"  //String
+let idade = 20  //number 
+let logado = true  //Boolean
+let lista = ["ovo", "massa", "corante"]  //array
+let nomeCompleto = {nome: "Matheus", idade: 20}  //Object
+
+console.log(typeof nome)
+console.log(typeof idade)
+console.log(typeof logado)
+console.log(typeof lista)
+console.log(typeof nomeCompleto)
+*/
+
+/* md01 aula 09
+
+
+let nome = "Matheus"; let idade = 20;
+
+console.log(nome); console.log(idade);*/
+
+
+
+/* md01 aula10
+let
+var
+const nome = "Matheus";
+console.log(nome)
+
+nome = "João"
+console.log(nome);*/
+
+
+
+/* md01 aula11
+
+let x = "2";
+
+x = x + 8;
+
+console.log(x);
+
+
+let nome = "Matheus";
+let sobreNome = "Portela";
+
+let nomeCompleto = nome + ' ' + sobreNome;
+
+console.log(nomeCompleto);
+
+
+let n1 = 15;
+let n2 = 20;
+let n3 = 10;
+
+let resultado = (n1 + n2) / n3;
+
+console.log(resultado)
+*/
+
+
+/* md01 aula12
+
+let nome = "Matheus";
+let sobreNome = "Portela";
+
+//let nomeCompleto = nome + ' ' + sobreNome;
+let nomeCompleto = `${nome} Marques ${sobreNome}` 
+
+console.log("Nome: " + nomeCompleto);
+
+
+
+let idade = 22;
+
+let idadeString = `idade: ${idade - 2} anos`;
+//let idadeString = 'Idade: ' + idade + ' anos'; o template string substitui esse modelo aqui
+
+console.log(idadeString);*/
+
+
+/*md01 aula13 */
+
+let idade = 17;
+
+if (idade >= 17) {
+    console.log("Você é maior de idade!")
+} else {
+    console.log("Você é menor de idade!")
+}
