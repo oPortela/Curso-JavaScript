fe9a0c33b223f4c8e8565472a6069148b2ab2be0
Matheus Marques <matheusmarquesportela@gmail.com>
2023-08-07 14:17:23 -0300

    Update script-md02.js

--- assets/js/script-md02.js
+++ assets/js/script-md02.js
@@ -11,7 +11,7 @@ console.log('Opa, tudo bem?');
 gravidade(); //para rodar a função basta colocar seu nome mais os parenteses
 */
 
-/* md02 aula 04 e 05*/
+/* md02 aula 04 e 05
 
 function somar(n1, n2) {
     let resultado = n1 + n2;
@@ -32,3 +32,23 @@ console.log('Nome completo: ' + completo);
 
 
 // ENTRADA -> PROCESSAMENTO -> SAIDA
+*/
+
+/* md02 aula 06 */
+
+function maiorDeIdade(idade) {
+    if (idade >= 18) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+let idade = 22;
+let verificacao = maiorDeIdade(idade);
+
+if(verificacao) {
+    console.log("É maior de idade!");
+} else {
+    console.log("É menor de idade!")
+}
