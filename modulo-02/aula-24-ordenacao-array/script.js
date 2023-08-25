8b85ff5cc71f3d0e1c01e0cadad7a9a2e1916877
Matheus Marques <matheusmarquesportela@gmail.com>
2023-08-25 14:53:35 -0300

    update

---
+++ modulo-02/aula-24-ordenacao-array/script.js
@@ -0,0 +1,34 @@
+/*let fruits = [
+    'Maça',
+    'Uva',
+    'Laranja',
+    'Banana'
+];
+
+fruits.sort(); /*Ele fica em ordem alfabética
+fruits.reverse();
+/* Combinando os dois ordenamos em ordem de Z a A. 
+
+console.log(fruits);
+*/
+
+let cars = [
+    { brand: 'Fiat', year: 2022},
+    { brand: 'Bmw', year: 2018},
+    { brand: 'Ferrari', year: 2020},
+    { brand: "ford", year: 1969}
+];
+
+cars.sort((a, b) => {
+    
+});
+
+console.log(cars);
+
+/*if(a.year > b.year) {
+        return 1;
+    } else if(a.year < b.year) {
+        return -1;
+    }else {
+        return 0;
+    }*/
