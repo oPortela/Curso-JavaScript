ca1649b4f582d775ed7ecc58099150d200ea772b
Matheus Marques <matheusmarquesportela@gmail.com>
2023-08-28 14:38:13 -0300

    update

---
+++ modulo-03/aula-02-03-selcionando-elementos/script.js
@@ -0,0 +1,7 @@
+//DOM = 'Document Object Model';
+
+let elementos = document.getElementsByTagName("h1");
+let paragrafos = document.getElementsByTagName('p');
+
+console.log(elementos[0]);
+console.log(paragrafos[0]);
