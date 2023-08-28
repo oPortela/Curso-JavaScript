67b7cea5f814d73a9b527298169069e38b258e7a
Matheus Marques <matheusmarquesportela@gmail.com>
2023-08-28 10:30:34 -0300

    finalização do modulo 2

--- modulo-02/aula-25-iteracao-array/script.js
+++ modulo-02/aula-25-iteracao-array/script.js
@@ -20,20 +20,68 @@ let bigFruits = fruits.filter((item) => item.length > 4);
 console.log(bigFruits);*/
 
 
-let fruits = [
+/*let fruits = [
     'Maça',
     'Uva',
     'Laranja',
-    'Banana'
+    'Banana', 
+    'Pêra'
 ];
 
-fruits.every((value) => {
-    return value.length > 3;
-    /*if(value.length > 3) {
+let ok = fruits.every((value) => {
+    //return value.length > 3;
+    if(value.length > 3) {
         return true;
     } else {
         return false;
-    }*/
+    }
 });
 
-console.log(value())
+if(ok) {
+    console.log('Todos são maiores que 3')
+} else {
+    console.log('não são todos maiores que 3')
+}
+*/
+
+
+/*let fruits = [
+    'Uva',
+    'Maça',
+    'Laranja',
+    'Banana', 
+    'Pêra'
+];
+
+let ok = fruits.some((value) => { //o some ao contrario do every, ele retorna TRUE quando um item ja satisfaz a minha condição (Como no caso, uma fruita tem mais de três letras, então logo ele retorna TRUE).
+    //return value.length > 3;
+    if(value.length > 3) {
+        return true;
+    } else {
+        return false;
+    }
+});
+
+if(ok) {
+    console.log('Algum item é maior do que 3')
+} else {
+    console.log('Nenhum item é maior do que 3')
+}
+*/
+
+
+let fruits = [
+    'Uva',
+    'Maça',
+    'Laranja',
+    'Banana', 
+    'Pêra'
+];
+
+
+
+if(fruits.includes('Uva')) { //o includes verfica se possui alguma string dentro de um array, se sim ele retorna TRUE
+    console.log('Tem uva sim')
+}else {
+    console.log('Não possui uva...')
+}
